7ab67f7de79b4651bc2f1a8e0cadd405529f4aac
papasmf1 <papasmf1@gmail.com>
2021-09-28 13:46:07 +0900

    두번째 클래스

--- class2.py
+++ class2.py
@@ -1,10 +1,13 @@
-# class1.py 
+# class2.py 
 #클래스를 정의(새로운 형식을 만들기)
 #원본은 1개 
 class Person:
+    #클래스에서 공유하는 변수값
+    num_person = 0 
     #생성자(초기화)메서드
     def __init__(self):
         self.name = "default name"
+        Person.num_person += 1 
     #인스턴스 메서드 
     def print(self):
         print("My name is {0}".format(self.name))
@@ -12,11 +15,6 @@ class Person:
 #인스턴스 생성(복사본은 여러개)
 p1 = Person()
 p2 = Person()
-p2.name = "전우치"
-p1.print()
-p2.print()
-#동적 형식을 언어여서 변수 추가가 자유롭다~~ 
-Person.title = "new title"
-print(p1.title)
-print(p2.title)
-print(Person.title)
+print("인스턴스의 갯수:{0}".format(Person.num_person) )
+
+
